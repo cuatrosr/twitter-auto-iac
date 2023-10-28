d29d6404d055c4f69022792694d7356ccc5fb725
David Montaño Tamayo <cuatro04sr@gmail.com>
2023-10-28 12:02:37 -0500

    feat: network configuration (#4)
    * feat: add initial network configuration
    
    * feat: add network security rules
    
    * fix: modify network interface variable tag name
    
    * fix: modify container registry variable name

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,47 @@
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = "autoinfraprod-rg"
+module "rg" {
+  source = "./modules/azurerm_resource_group"
 }
 
-resource "azurerm_storage_account" "sa" {
-  name                     = "autoinfratwitterprod"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.resource_group_location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+module "vnet" {
+  source      = "./modules/azurerm_virtual_network"
+  rg_name     = module.rg.rg_name
+  rg_location = module.rg.rg_location
 }
 
-resource "azurerm_storage_container" "sc" {
-  name                  = "content"
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "private"
+module "snet" {
+  source    = "./modules/azurerm_subnet"
+  rg_name   = module.rg.rg_name
+  vnet_name = module.vnet.vnet_name
+}
+
+module "pubip" {
+  source      = "./modules/azurerm_public_ip"
+  rg_name     = module.rg.rg_name
+  rg_location = module.rg.rg_location
+}
+
+module "ni" {
+  source      = "./modules/azurerm_network_interface"
+  rg_name     = module.rg.rg_name
+  rg_location = module.rg.rg_location
+  sn_id       = module.snet.snet_id
+  pubip_id    = module.pubip.pubip_id
+}
+
+module "nsg" {
+  source      = "./modules/azurerm_network_security_group"
+  rg_name     = module.rg.rg_name
+  rg_location = module.rg.rg_location
+}
+
+module "nisga" {
+  source = "./modules/azurerm_network_interface_security_group_association"
+  ni_id  = module.ni.ni_id
+  nsg_id = module.nsg.nsg_id
+}
+
+module "cr" {
+  source      = "./modules/azurerm_container_registry"
+  rg_name     = module.rg.rg_name
+  rg_location = module.rg.rg_location
 }

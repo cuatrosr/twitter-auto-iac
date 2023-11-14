1416bc0b43b110f500ccb0034ccf2659269ac164
David Montaño Tamayo <cuatro04sr@gmail.com>
2023-11-14 17:52:07 -0500

    feat: add helm first approach

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,3 +89,17 @@ module "cg" {
   sa_name     = module.sa.sa_name
   sa_key      = module.sa.sa_key
 }
+
+resource "helm_release" "kube-prometheus" {
+  name       = "kube-prometheus-stackr"
+  namespace  = "monitoring"
+  version    = "36.2.0"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+}
+
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,6 +10,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~>2.11.0"
+    }
   }
 }
 
@@ -26,4 +30,14 @@ data "azurerm_kubernetes_cluster" "cluster" {
 
 provider "azurerm" {
   features {}
+}
+
+provider "helm" {
+  kubernetes {
+    host = data.azurerm_kubernetes_cluster.cluster.kube_config.0.host
+
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+  }
 }

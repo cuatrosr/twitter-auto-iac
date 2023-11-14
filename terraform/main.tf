42cbab0891a9a073dc3d0460507f58e256b776c8
David Montaño Tamayo <cuatro04sr@gmail.com>
2023-11-14 17:57:19 -0500

    fix: namespace

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,14 +92,8 @@ module "cg" {
 
 resource "helm_release" "kube-prometheus" {
   name       = "kube-prometheus-stackr"
-  namespace  = "monitoring"
+  namespace  = "kube-public"
   version    = "36.2.0"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
-}
-
-resource "kubernetes_namespace" "monitoring" {
-  metadata {
-    name = "monitoring"
-  }
 }

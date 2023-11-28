fb511e9ea309ed16138ad15ae69f34d7090f0704
David Montaño Tamayo <cuatro04sr@gmail.com>
2023-11-28 11:29:03 -0500

    feat: removed mongo

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,26 +87,3 @@ module "helm" {
   depends_on = [module.aks]
   source     = "./modules/helm/release"
 }
-
-resource "mongodbatlas_cluster" "test" {
-  project_id   = "656611ec1f93e97aa25af3cb"
-  name         = "test"
-  cluster_type = "REPLICASET"
-  replication_specs {
-    num_shards = 1
-    regions_config {
-      region_name     = "US_EAST"
-      electable_nodes = 3
-      priority        = 7
-      read_only_nodes = 0
-    }
-  }
-  cloud_backup                 = true
-  auto_scaling_disk_gb_enabled = true
-  mongo_db_major_version       = "4.2"
-
-  # Provider Settings "block"
-  provider_name               = "AZURE"
-  provider_disk_type_name     = "P6"
-  provider_instance_size_name = "M30"
-}
